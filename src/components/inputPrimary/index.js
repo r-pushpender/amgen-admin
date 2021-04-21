dfc75d07e28c0eec39307dd9cc6c58835c622d36
Pushpender Rana <pushpender661@gmail.com>
2021-04-21 18:18:18 +0530

    Add formik fields

--- src/components/inputPrimary/index.js
+++ src/components/inputPrimary/index.js
@@ -1,24 +1,36 @@
 // Styles
 import "./styles.scss";
 
-function InputPrimary(props) {
+function InputPrimary({
+  field, // { name, value, onChange, onBlur }
+  //form: { touched, errors }, // also values, setXXXX, handleXXXX, dirty, isValid, status, etc.
+  ...props
+}) {
   return (
     <div className={`input-style ${props.inputStyle}`}>
       <div className={`label-style ${props.labelStyle}`}>{props.label}</div>
       {props.type === "textarea" ? (
-        <textarea
-          row={props.row}
-          column={props.column}
-          placeholder={props.placeholder}
-          type={props.type}
-        />
+        <div>
+          <textarea
+            row={props.row}
+            column={props.column}
+            placeholder={props.placeholder}
+            type={props.type}
+            {...field}
+          />
+
+
+        </div>
       ) : (
-        <input
-          placeholder={props.placeholder}
-          value={props.value}
-          onChange={props.onChange}
-          type={props.type}
-        />
+        <div>
+          <input
+            placeholder={props.placeholder}
+            value={props.value}
+            onChange={props.onChange}
+            type={props.type}
+            {...field}
+          />
+        </div>
       )}
       <p className={props.errorClass}>{props.error}</p>
     </div>
--- src/containers/auth/login/index.js
+++ src/containers/auth/login/index.js
@@ -1,9 +1,10 @@
-import {Link, useHistory} from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 import logoImage from "../../../assets/images/logo.png";
 import ButtonPrimary from "../../../components/buttons/buttonPrimary";
 import CheckboxPrimary from "../../../components/checkboxPrimary";
 import InfoMessage from "../../../components/infoMessage";
 import InputPrimary from "../../../components/inputPrimary";
+import { Formik, Field } from 'formik';
 
 // Styles
 import "./styles.scss";
@@ -26,19 +27,40 @@ function Login() {
                 <h4 className="title3">Sign In</h4>
               </div>
 
-              <div className="form-section mt--45">
-                <InputPrimary placeholder="Enter Email Address" label="Email" />
-                <InputPrimary placeholder="Enter Password" label="Password" />
-                <CheckboxPrimary label="Keep me signed in" />
+              <Formik
+                enableReinitialize
+                initialValues={{
+                  username: ''
+                }}
+                onSubmit={() => {
+                  console.log("submit");
+                }}
+              >
+                {({ values, setFieldValue, submitForm }) => (
+                  <form>
+                    <div>{JSON.stringify(values)}</div>
+                    <div className="form-section mt--45">
+                      <Field
+                        component={InputPrimary}
+                        name="username"
+                        value={values.username}
+                        placeholder="Enter your username" label="Username"
+                      />
+                      <InputPrimary placeholder="Enter Email Address" label="Email" />
+                      <InputPrimary placeholder="Enter Password" label="Password" />
+                      <CheckboxPrimary label="Keep me signed in" />
 
-                <ButtonPrimary title="Sign In" onClick={handleClick} />
+                      <ButtonPrimary title="Sign In" onClick={() => submitForm} />
 
-                <div className="text-center mt--25">
-                  <Link className="btn-link" to="/password-reset">
-                    Forgot your password?
+                      <div className="text-center mt--25">
+                        <Link className="btn-link" to="/password-reset">
+                          Forgot your password?
                   </Link>
-                </div>
-              </div>
+                      </div>
+                    </div>
+                  </form>
+                )}
+              </Formik>
             </div>
           </div>
         </div>
